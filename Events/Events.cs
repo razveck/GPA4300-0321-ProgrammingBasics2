afee8e6886e2e8f75d1f999ccf2c1c9a2d0e6bc6
razveck <razveck@gmail.com>
2021-07-01 23:43:14 +0200

    Lecture 2: properties, delegates and events

---
+++ Events/Events.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Events {
+
+	//class type
+	class Player{ }
+
+	//delegate type
+	delegate void Callback();
+
+	delegate bool ValidationDelegate(string value);
+
+
+	class Events {
+		static void Main(string[] args) {
+			//using the class type
+			Player player1 = new Player();
+
+			Callback callback = new Callback(PrintMyName);
+			callback = PrintMyName;
+
+			callback();
+
+			Computer computer = new Computer();
+			computer.Callback = PrintMyName;
+			computer.ValidationDelegate = IsValid;
+
+
+			//----EVENTS----
+			//we could implement our own events like this
+			
+			computer.callbackEvent.Add(PrintMyName);
+			computer.callbackEvent.Add(PrintMyName);
+			computer.callbackEvent.Add(Foo);
+
+			foreach(var func in computer.callbackEvent) {
+				func();
+			}
+			computer.RunSomePrograms();
+
+			//or we could use the C# built-in events
+			//subscribing to the event
+			computer.ProgramsFinished += PrintMyName;
+			computer.ProgramsFinished += PrintMyName;
+			computer.ProgramsFinished += Foo;
+
+			//can only raise/invoke/trigger/call the event within the declaring class
+			//computer.ProgramsFinished();
+
+			computer.ActionDelegate = Foo;
+			computer.FloatDelegate = Bar;
+			computer.ValidateDelegate = IsValid;
+
+		}
+
+		static void PrintMyName(){
+			Console.WriteLine("Joao");
+		}
+
+		static void Foo(){
+
+		}
+
+		static void Bar(float a){
+
+		}
+
+		static bool IsValid(string value){
+			return !string.IsNullOrEmpty(value);
+		}
+	}
+
+	class Computer{
+		public Callback Callback;
+		public ValidationDelegate ValidationDelegate;
+
+		public List<Callback> callbackEvent = new List<Callback>();
+		public event Callback ProgramsFinished;
+
+		public Action ActionDelegate;
+		public Action<float> FloatDelegate;
+
+		public Func<string, bool> ValidateDelegate;
+
+		public void RunSomePrograms(){
+			//do stuff here
+			//...
+
+			if(Callback != null)
+				Callback();
+
+			bool isValid = ValidationDelegate("");
+
+			//this could throw an error, if nothing is subscribed
+			//ProgramsFinished();
+			//ProgramsFinished.Invoke();
+			
+			if(ProgramsFinished != null)
+				ProgramsFinished.Invoke(); //invoke is the same as just using the parenthesis
+
+			//same thing as the previous null check
+			ProgramsFinished?.Invoke();
+		}
+	}
+}
---
+++ Properties/Properties.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace Properties {
+
+	class Person {
+		private string _name;
+
+		public void SetName(string value) {
+			if(string.IsNullOrEmpty(value))
+				return; //do something, throw an error
+
+			_name = value;
+			Console.WriteLine($"Name is now {value}");
+		}
+
+		public string GetName() {
+			return _name;
+		}
+
+		public string Name
+		{
+			get
+			{
+				return _name;
+			}
+			set
+			{
+				if(string.IsNullOrEmpty(value))
+					return; //do something, throw an error
+
+				_name = value;
+				Console.WriteLine($"Name is now {value}");
+			}
+		}
+
+		//backing field
+		private int _age;
+
+		//full property
+		public int Age
+		{
+			set { }
+			get { return 1; }
+		}
+
+		//auto-property
+		public string AutoProperty { get; set; }
+
+		public string ReadOnlyProperty { get; private set; }
+		public string WriteOnlyProperty { private get; set; }
+
+		public readonly string ReadOnlyVariable = "uihu";
+
+		void Foo() {
+			ReadOnlyProperty = "jj";
+			//not allowed
+			//ReadOnlyVariable = "olkok";
+		}
+
+		public Person() {
+			ReadOnlyVariable = "ok";
+		}
+
+		public int InitializedProperty { get; set; } = 10;
+
+		public void DoTheThing(int howManyThings) => Console.WriteLine($"Doing {howManyThings} things!");
+
+		public int PropertyWithArrows
+		{
+			get => 4238;
+			set => Console.WriteLine(value);
+		}
+
+		private int _getterOnly;
+		//public int GetterOnlyProperty { get => _getterOnly; }
+		public int GetterOnlyProperty => _getterOnly;
+	}
+
+	class Properties {
+		static void Main(string[] args) {
+			Person a = new Person();
+			a.SetName("Joao");
+			Console.WriteLine(a.GetName());
+
+			a.Name = "Masterchief";
+			Console.WriteLine(a.Name);
+
+			a.Age = 10;
+			a.Age = 20;
+
+			//inaccessible, the setter is private
+			//a.ReadOnlyProperty = "njuh";
+			//we can still read it and use it
+			Console.WriteLine(a.ReadOnlyProperty);
+
+			//inaccessible, the getter is private
+			//Console.WriteLine(a.WriteOnlyProperty);
+			//we can still write to it
+			a.WriteOnlyProperty = "Something";
+
+			a.DoTheThing(200);
+			a.PropertyWithArrows = 666;
+
+		}
+	}
+}
