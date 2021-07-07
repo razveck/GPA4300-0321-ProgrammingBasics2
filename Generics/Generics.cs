3a045eadeafb52d5d6712f6ff7b844e037cf1c6d
razveck <razveck@gmail.com>
2021-07-07 12:45:29 +0200

    Lecture 3: Generics and Reference Parameters

---
+++ Generics/Generics.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics {
+
+	//in C++ it's called templates
+	//templated types
+
+	class Generics {
+		static void Main(string[] args) {
+			List<int> intList = new List<int>();
+
+			//the generic type MUST be known at compile time
+
+			AaronPaul<bool> paul = new AaronPaul<bool>();
+			paul.TryToEat();
+			paul.TheWhatNow(true);
+
+			AaronPaul<List<int>> listPaul = new AaronPaul<List<int>>();
+			listPaul.TheWhatNow(intList);
+			listPaul.TryToEat();
+
+			AaronPaul<MyStruct> structPaul = new AaronPaul<MyStruct>();
+
+			//GetComponent<int>(1);
+			GetComponent<Collider>();
+
+			Foo<int, bool, int, Component, string>();
+
+			Collider c;
+			if(TryGetComponent<Collider>(out c)){
+				//do stuff
+			}
+			//can use the c variable
+			c = null;
+
+			//in this case, the compiler extracts the col variable into a local variable
+			if(TryGetComponent(out Collider col)){
+
+			}
+			//can use the col variable
+			col = null;
+		}
+
+		static T GetComponent<T>() where T : Component
+			//,new()
+			{
+			//T t = new T();
+			return default;
+		}
+
+		static T Foo<T, U, V, T1, T2>(){
+			return default;
+		}
+
+		static bool TryGetComponent<T>(out T component) where T : Component{
+			//if the component was found, return true
+			component = default;
+
+			//if not, return false
+
+
+			return true;
+
+		}
+	}
+
+	class Component {
+		public Component(int a) {
+
+		}
+	}
+
+	class Collider : Component {
+		public Collider(int a, int b) : base(a) {
+
+		}
+	}
+
+	struct MyStruct {
+
+	}
+
+	class AaronPaul<T>
+		//we can restrict what T can be, by using the where keyword
+		//where T: class
+		//where T: struct
+		{
+		public T What;
+
+		public AaronPaul() {
+			//cannot ensure that T is a nullable type
+			//What = null;
+		}
+
+		public void TryToEat() {
+			Random rand = new Random();
+			int coinFlip = rand.Next(0, 2);
+			if(coinFlip == 0) {
+				Console.WriteLine($"AaronPaul ate the {What.GetType()}");
+			} else {
+				Console.WriteLine($"AaronPaul threw the {What.GetType()} down the toilet");
+			}
+		}
+
+		public T ReturnWhat() {
+			return What;
+		}
+
+		public void TheWhatNow(T that) {
+			What = that;
+		}
+	}
+}
---
+++ ReferenceParameters/ReferenceParameters.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReferenceParameters {
+
+	class Player{
+		public int Health;
+		public string Name;
+	}
+
+	struct Vector2{
+		public float X;
+		public float Y;
+	}
+
+	class ReferenceParameters {
+		static void Main(string[] args) {
+			string myString = "A";
+			Foo(myString);
+			FooRef(ref myString);
+			FooOut(out myString);
+			FooIn(in myString);
+		}
+
+		static void Foo(string name){
+			name = "B";
+		}
+
+		//REF - parameter can be changed and it changes the variable in the calling scope
+		//can be changed, but doesn't have to
+		static void FooRef(ref string name){
+			name = "C";
+		}
+
+		//OUT - parameter MUST be assigned to IN THE FUNCTION, it changes the calling scope
+		static void FooOut(out string name){
+			name = "D";
+		}
+
+		//out enforces assignment of the parameter
+		static void ReturnPlayer(out string playerName){
+			playerName = "Mario";
+		}
+
+		//IN - parameter CANNOT be changed
+		static void FooIn(in string name){
+			//compiler error, can't change the in parameter
+			//name = "E";
+		}
+
+		//first use case: disallow changing the reference, but still allow using its members
+		static void ChangeHealth(in Player player){
+			//player = new Player();
+			player.Health += 10;
+		}
+
+		//second use case: disallow changing structs or members of structs
+		static void UseVector2(in Vector2 vector){
+			//cannot change members of struct, because that would require the reference to the struct to be changed
+			//vector.X = 10f;
+		}
+	}
+}
